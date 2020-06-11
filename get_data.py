84b88c818d7188810b5cb936c8fd0ec92f4e1c99
Jacob <jacobjennings510@gmail.com>
2020-06-11 12:35:10 -0700

    Set data collection interval 10 mins

--- get_data.py
+++ get_data.py
@@ -8,11 +8,10 @@ def getData():
     humidity, temperature = dht.read_retry(sensor, pin)
     if humidity is not None and temperature is not None:
         print('Temp={0:0.1f}*C Humidity={1:0.1f}%'.format(temperature, humidity))
-        # time.sleep(60) # Get reading every 1 minute(s) (leave on overnight to test)
     else:
         print('Failed to get reading')
 
-    time.sleep(60)
+    time.sleep(600) # Get reading every 10 minutes (leave on overnight to test)
     return temperature, humidity
         
         
