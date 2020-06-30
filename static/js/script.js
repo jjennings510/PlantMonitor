a204412b40d3e7689d7dea76ce57afdf928ca59c
Jacob <jacobjennings510@gmail.com>
2020-06-30 12:54:50 -0700

    separated charts

--- static/js/script.js
+++ static/js/script.js
@@ -10,6 +10,8 @@ var light = 0;
 console.log("initiated variables");
 
 var ctx = $("#temperature_chart");
+var ctx2 = $('#moisture_chart');
+
 console.log("test");
 var temperature_chart = new Chart(ctx, {
 
@@ -26,26 +28,6 @@ var temperature_chart = new Chart(ctx, {
                 borderWidth: 3,
                 fill: false,
                 yAxisID: "temperature"
-            },
-            
-
-            {
-                label: 'Humidity',
-                data: [humidity],
-                borderColor: [
-                    '#d6c73e'
-                ],
-                fill: false,
-                yAxisID: "humidity"
-            }, 
-            {
-                label: 'Soil Moisture',
-                data: [moisture],
-                borderColor: [
-                    '#823abc'
-                ],
-                fill: false,
-                yAxisID: "moisture"
             },
             {
                 label: "Light Intesity",
@@ -79,18 +61,76 @@ var temperature_chart = new Chart(ctx, {
                 display: true,
                 position: 'left',
                 ticks: {
-                    suggestedMin: 15,
-                    suggestedMax: 30
+                    suggestedMin: 30,
+                    suggestedMax: 110
                     },
                 scaleLabel : {
                     display: true,
-                    labelString: 'Temperature (C)'
+                    labelString: 'Temperature (F)'
                     }
                 },
                 {
+                    id: "light",
+                    display: true,
+                    position: 'right',
+                    ticks: {
+                        suggestedMin: 0,
+                        suggestedMax: 1000
+                    },
+                    scaleLabel: {
+                        display: true,
+                        labelString: 'Light Intensity'
+                    }
+                }]
+            }
+
+    }
+
+});
+
+var moisture_chart = new Chart(ctx2, {
+    type: 'line',
+    data: {
+        labels: [],
+        datasets: [
+            {
+                label: 'Humidity',
+                data: [humidity],
+                borderColor: [
+                    '#d6c73e'
+                ],
+                fill: false,
+                yAxisID: "humidity"
+            },
+            {
+                label: 'Soil Moisture',
+                data: [moisture],
+                borderColor: [
+                    '#823abc'
+                ],
+                fill: false,
+                yAxisID: "moisture"
+            }
+        ]
+    },
+    options: {
+        responsive: false,
+        scales: {
+            xAxes: [ {
+                display: true,
+                scaleLabel : {
+                    display: true,
+                    labelString: 'Time of day'
+                    },
+                ticks: {
+                   autoSkip: true,
+                   maxTicksLimit: 12
+                }
+            }],
+            yAxes: [ {
                 id: "humidity",
                 display: true,
-                position: 'right',
+                position: 'left',
                 ticks: {
                     suggestedMin: 0,
                     suggestedMax: 100
@@ -112,25 +152,11 @@ var temperature_chart = new Chart(ctx, {
                         display: true,
                         labelString: 'Soil Moisture Percentage'
                     }
-                },
-                {
-                    id: "light",
-                    display: true,
-                    position: 'left',
-                    ticks: {
-                        suggestedMin: 0,
-                        suggestedMax: 1000
-                    },
-                    scaleLabel: {
-                        display: true,
-                        labelString: 'Light Intensity'
-                    }
-                }]
-            }
-
+                }
+            ]
+        }
     }
-
-});
+})
 
 var x = 0;
 var temperature = 0;
@@ -138,14 +164,25 @@ var humidity = 0;
 
 var updated_data = $.get('/update');
 updated_data.done(function(results) {
-    temperature = results.results[0];
-    humidity = results.results[1];
-    x = results.results[2];
+    // Get results
+    humidity = results.results[0];
+    temperature = results.results[1];
+    moisture = results.results[2];
+    light = results.results[3];
+    x = results.results[4];
     console.log(temperature);
     console.log(humidity);
+
+    // Update Temp Chart
     temperature_chart.data.datasets[0].data = temperature;
-    temperature_chart.data.datasets[1].data = humidity;
+    temperature_chart.data.datasets[1].data = light;
     temperature_chart.data.labels = x;
     temperature_chart.update();
+
+    // Update Moisture Chart
+    moisture_chart.data.datasets[0].data = humidity;
+    moisture_chart.data.datasets[1].data = moisture;
+    moisture_chart.data.labels = x;
+    moisture_chart.update();
 
 });
